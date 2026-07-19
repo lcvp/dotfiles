9171382660d4d32085ec60cb116f60b1b7b060f0
lcvp <221131646+lcvp@users.noreply.github.com>
2026-07-19 09:00:28 +0700

    feat(nvim): remove conform.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,15 +6,12 @@ vim.pack.add({
 	{ src = "https://github.com/neovim/nvim-lspconfig" },
 	{ src = "https://github.com/mason-org/mason.nvim" },
 	{ src = "https://github.com/mason-org/mason-lspconfig.nvim" },
-	{ src = "https://github.com/stevearc/conform.nvim" },
 	{ src = "https://github.com/nvim-lua/plenary.nvim" },
 	{ src = "https://github.com/nvim-telescope/telescope.nvim" },
 })
 
 require("mason").setup()
 require("mason-lspconfig").setup()
-
-require("conform").setup({ format_on_save = { lsp_format = "fallback" } })
 
 require("telescope").setup({ defaults = { path_display = { "truncate" } } })
 vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files)
