b302878caedbccf2db6dfa553fbb298ab3eda10b
lcvp <221131646+lcvp@users.noreply.github.com>
2026-07-24 16:19:31 +0700

    feat(nvim): remove telescope.nvim (#3)

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,12 +6,7 @@ vim.pack.add({
 	{ src = "https://github.com/neovim/nvim-lspconfig" },
 	{ src = "https://github.com/mason-org/mason.nvim" },
 	{ src = "https://github.com/mason-org/mason-lspconfig.nvim" },
-	{ src = "https://github.com/nvim-lua/plenary.nvim" },
-	{ src = "https://github.com/nvim-telescope/telescope.nvim" },
 })
 
 require("mason").setup()
 require("mason-lspconfig").setup()
-
-require("telescope").setup({ defaults = { path_display = { "truncate" } } })
-vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files)
