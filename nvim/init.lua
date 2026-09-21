c41ca449f5f026dfdbe873dec5fe81cbf58c8bca
lcvp <221131646+lcvp@users.noreply.github.com>
2026-09-22 06:44:10 +0700

    style(nvim): simplify init.lua (#7)

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,7 @@
 vim.pack.add({
-	{ src = "https://github.com/neovim/nvim-lspconfig" },
-	{ src = "https://github.com/mason-org/mason.nvim" },
-	{ src = "https://github.com/mason-org/mason-lspconfig.nvim" },
+	"https://github.com/neovim/nvim-lspconfig",
+	"https://github.com/mason-org/mason.nvim",
+	"https://github.com/mason-org/mason-lspconfig.nvim",
 })
 
 require("mason").setup()
